0170008d8f712bd2d21b2164a6f4cf24e932fe5e
rek <orsitoth@ymail.com>
2017-10-28 13:34:26 +0200

    jav: kinezet

--- js/match.js
+++ js/match.js
@@ -8,7 +8,7 @@ getJson(
     }
 );
 
-
+document.querySelector("#title").innerHTML = "Az alábbiakban a 2017-18 évi osztrák labdarúgó bajnokság " + code + " kódú csapatának meccsei láthatók";
 
 function fillTable(rows) {
     var table = document.querySelector("table");
@@ -18,7 +18,7 @@ function fillTable(rows) {
             if (rows[i].matches[j].team1.code == code) {
                 var tr = "<tr>";
                 tr += "<td>" + rows[i].name.split(" ")[1] + "</td>";
-                tr += "<td>" + rows[i].matches[j].date + "</td>";
+                tr += "<td>" + rows[i].matches[j].date.split("-")[0] + ". " + rows[i].matches[j].date.split("-")[1] + ". " + rows[i].matches[j].date.split("-")[2] + "." + "</td>";
                 tr += "<td>" + rows[i].matches[j].team2.name + "</td>";
                 tr += "<td>" + rows[i].matches[j].score1 + " - " + rows[i].matches[j].score2 + "</td>";
                 tr += "</tr>";
@@ -27,7 +27,7 @@ function fillTable(rows) {
             } else if (rows[i].matches[j].team2.code == code) {
                 var tr = "<tr>";
                 tr += "<td>" + rows[i].name.split(" ")[1] + "</td>";
-                tr += "<td>" + rows[i].matches[j].date + "</td>";
+                tr += "<td>" + rows[i].matches[j].date.split("-")[0] + ". " + rows[i].matches[j].date.split("-")[1] + ". " + rows[i].matches[j].date.split("-")[2] + "." + "</td>";
                 tr += "<td>" + rows[i].matches[j].team1.name + "</td>";
                 tr += "<td>" + rows[i].matches[j].score2 + " - " + rows[i].matches[j].score1 + "</td>";
                 tr += "</tr>";
